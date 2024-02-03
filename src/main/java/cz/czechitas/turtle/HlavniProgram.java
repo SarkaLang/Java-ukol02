a27a795b087c3d22d24fa5d10c7fb3f6f4372f96
SarkaLang <sarka.langy@gmail.com>
2024-02-03 10:16:00 +0100

    Příprava metod

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
@@ -10,7 +12,23 @@ public class HlavniProgram {
     }
 
     public void start() {
-        //TODO Tady bude kód pro kreslení želví grafiky.
+        // barvy
+        Color zelenaBarva;
+        zelenaBarva = new Color(42, 170, 138);
+
+        Color zlutaBarva;
+        zlutaBarva = new Color(255, 255, 0);
+
+        Color sedaBarva;
+        sedaBarva = new Color(78, 75, 75);
+
+        // obrazce
+        // nakresliRovnostrannyTrojuhelnik(50.0, zelenaBarva);
+
+        // nakresliDelsiStranu(70.0);
+        // nakresliKratsiStranu(30.0);
+
+        //nakresliKolecko(4, zlutaBarva);
     }
 
 }
