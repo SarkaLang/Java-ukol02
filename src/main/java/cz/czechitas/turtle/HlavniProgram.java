b80f3b7d5cfe9d30a707badb41e06d820fa7b1e2
SarkaLang <sarka.langy@gmail.com>
2024-02-03 11:15:50 +0100

    Kornout + sněhulák

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -16,19 +16,119 @@ public class HlavniProgram {
         Color zelenaBarva;
         zelenaBarva = new Color(42, 170, 138);
 
-        Color zlutaBarva;
-        zlutaBarva = new Color(255, 255, 0);
+        Color modraBarva;
+        modraBarva = new Color(80, 194, 236);
 
         Color sedaBarva;
         sedaBarva = new Color(78, 75, 75);
 
         // obrazce
-        // nakresliRovnostrannyTrojuhelnik(50.0, zelenaBarva);
+        nakresliKolecko(4, modraBarva);
+        nakresliRovnostrannyTrojuhelnik(150, zelenaBarva);
 
+        nakresliKoleckoJedna(4, sedaBarva);
+        nakresliKoleckoDva(5, modraBarva);
+        nakresliKoleckoTri(7, zelenaBarva);
+        nakresliKoleckoCtyri(2, zelenaBarva);
+        nakresliKoleckoPet(2, zelenaBarva);
         // nakresliDelsiStranu(70.0);
         // nakresliKratsiStranu(30.0);
 
-        //nakresliKolecko(4, zlutaBarva);
     }
 
+    public void prvniPohyb() {
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(400);
+        zofka.turnRight(90);
+        zofka.penDown();
+    }
+
+    public void nakresliKolecko(double delkaStrany, Color barva) {
+        prvniPohyb();
+
+        for (int i = 0; i < 107; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(5);
+        }
+
+        zofka.turnRight(10);
+    }
+
+    public void nakresliRovnostrannyTrojuhelnik(double delkaStrany, Color barva) {
+        zofka.setPenColor(barva);
+
+        zofka.turnRight(10);
+        zofka.move(delkaStrany);
+        zofka.turnRight(145);
+        zofka.move(delkaStrany);
+        zofka.turnRight(110);
+        zofka.move(80);
+
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.penDown();
+    }
+
+    public void nakresliKoleckoJedna(double delkaStrany, Color barva) {
+        for (int i = 0; i < 123; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(5);
+        }
+
+        zofka.turnLeft(150);
+    }
+
+    public void nakresliKoleckoDva(double delkaStrany, Color barva) {
+        for (int i = 0; i < 102; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(5);
+        }
+        zofka.turnLeft(150);
+    }
+
+    public void nakresliKoleckoTri(double delkaStrany, Color barva) {
+        for (int i = 0; i < 72; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(5);
+        }
+
+        zofka.turnLeft(105);
+
+        zofka.penUp();
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+    }
+
+    public void nakresliKoleckoCtyri(double delkaStrany, Color barva) {
+        for (int i = 0; i < 90; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(5);
+        }
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(170);
+        zofka.turnRight(70);
+        zofka.penDown();
+    }
+
+    public void nakresliKoleckoPet(double delkaStrany, Color barva) {
+        for (int i = 0; i < 85; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnLeft(5);
+        }
+        zofka.penUp();
+        zofka.move(150);
+        zofka.turnRight(85);
+    }
 }
